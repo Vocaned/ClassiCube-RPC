e232d420ebc3562995023cb7e7baea1fd4c0ac77
Vocaned <10358674+Vocaned@users.noreply.github.com>
2020-04-14 18:17:35 +0300

    Fix compiling script

--- misc/build.sh
+++ misc/build.sh
@@ -1,33 +1,32 @@
 WIN32=i686-w64-mingw32
 WIN64=x86_64-w64-mingw32
+NIX64=gcc
 
 FLAGS="-shared -fPIC -O1 -s"
-FLAGS_NIX64="-Wl,-rpath='$ORIGIN/..'"
 
-WIN64_CLIENT="cc-w64-d3d"
-WIN32_CLIENT="cc-w32-d3d"
-
-WIN64_LIBS="-L. -Ldiscord_game_sdk/lib/x86_64 -l$WIN64_CLIENT -ldiscord_game_sdk"
-WIN32_LIBS="-L. -Ldiscord_game_sdk/lib/x86 -l$WIN32_CLIENT -ldiscord_game_sdk"
+WIN64_LIBS="-L. -Ldiscord_game_sdk/lib/x86_64 -lClassiCube -ldiscord_game_sdk"
+WIN32_LIBS="-L. -Ldiscord_game_sdk/lib/x86 -lClassiCube -ldiscord_game_sdk"
 NIX64_LIBS="-Ldiscord_game_sdk/lib/x86_64 -l:discord_game_sdk.so"
 
 cd ..
 mkdir -p out
 
 echo "Compiling Nix64 Plugin"
-gcc -m64 Discord.c -o Discord.so $FLAGS $FLAGS_NIX64
+$NIX64 -m64 Discord.c -o Discord.so $FLAGS $NIX64_LIBS -Wl,-rpath='$ORIGIN/..'
 mv Discord.so out
 
 echo "Compiling Win32 Plugin"
-gendef $WIN32_CLIENT.exe
-$WIN32-dlltool -d $WIN32_CLIENT.def -l lib$WIN32_CLIENT.a -D $WIN32_CLIENT.exe
+cp cc-w32-d3d.exe ClassiCube.exe
+gendef ClassiCube.exe
+$WIN32-dlltool -d ClassiCube.def -l libClassiCube.a -D ClassiCube.exe
 $WIN32-gcc Discord.c -o Discord_32.dll $FLAGS $WIN32_LIBS
 mv Discord_32.dll out
-rm $WIN32_CLIENT.def lib$WIN32_CLIENT.a
+rm ClassiCube.def libClassiCube.a ClassiCube.exe
 
 echo "Compiling Win64 Plugin"
-gendef $WIN64_CLIENT.exe
-$WIN64-dlltool -d $WIN64_CLIENT.def -l lib$WIN64_CLIENT.a -D $WIN64_CLIENT.exe
+cp cc-w64-d3d.exe ClassiCube.exe
+gendef ClassiCube.exe
+$WIN64-dlltool -d ClassiCube.def -l libClassiCube.a -D ClassiCube.exe
 $WIN64-gcc Discord.c -o Discord.dll $FLAGS $WIN64_LIBS
 mv Discord.dll out
-rm $WIN64_CLIENT.def lib$WIN64_CLIENT.a
+rm ClassiCube.def libClassiCube.a ClassiCube.exe
